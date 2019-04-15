9b77f7228e9b8f4cbaaa2a5f6e2edd2cf5f1268e
Lukas <lukas@rosbergs.se>
2019-04-15 22:40:21 +0200

    lagt till databasfunktioner

---
+++ Hemsidan/Java/DatabaseConnection.java
@@ -0,0 +1,91 @@
+package com.login;
+
+import java.sql.*;
+import java.util.List;
+
+import domain.Product;
+
+public class DatabaseConnection {
+	
+	/*- variables
+	 * 
+	 * 
+	 */
+
+	String jdbUrl = "jdbc:postgresql://localhost:5432/postgres";
+	String username = "postgres";
+	String password = "password";
+//	
+//	// 
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+//	
+	public DatabaseConnection() {
+		
+	}
+//	
+	public void connect() {
+		try {
+			conn = DriverManager.getConnection(jdbUrl, username, password);
+			
+			System.out.println("Database connection established");
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+//	
+	public void disconnect() {
+		try {
+			if(stmt!=null) {
+				stmt.close();
+			}
+			
+			if (rs!=null) {
+				rs.close();
+			}
+			
+			if(conn!=null) {
+				conn.close();
+			}
+		}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+	
+
+}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome!");
+//		
+		DatabaseController ctrl = new DatabaseController();
+//		
+		ctrl.connect();
+//		
+		ctrl.createTables();
+//		coffeeCornerDb.setup();
+//		
+//		List<Product> allProducts = ctrl.selectAllProducts();
+//		int i = 0;
+//		
+//		while (i < allProducts.size()) {
+//			System.out.print("ID = " + allProducts.get(i).getProductId());
+//			System.out.print("Name= " + allProducts.get(i).getProductName());
+//			System.out.println("Price= " + allProducts.get(i).getProductPrice());
+//			i++;
+//		}
+		
+//		
+//		ctrl.updateProduct(2, "asdasd", 1.30);
+//		
+//		ctrl.updateProduct(5, "asd", -1.0);
+//		
+		ctrl.disconnect();
+
+	}
+}
+	
+	
---
+++ Hemsidan/Java/DatabaseController.java
@@ -0,0 +1,134 @@
+package com.login;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Product;
+
+public class DatabaseController extends DatabaseConnection {
+	
+	public void createTables() {
+		try {
+			stmt = conn.createStatement();
+			
+			String sql = "CREATE TABLE IF NOT EXISTS products2 (" +
+					"username VARCHAR(100) NOT NULL, " +
+					"password VARCHAR(100) NOT NULL)";
+			
+			stmt.executeUpdate(sql);
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+//	public void setup() {
+//		//count rows
+//		int numberOfRows = -1;
+//		try {
+//			PreparedStatement st = conn.prepareStatement(
+//					"SELECT count (*) FROM products");
+//			
+//			ResultSet rs = st.executeQuery();
+//			
+//			while(rs.next()) {
+//				numberOfRows = rs.getInt(1);
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		if(numberOfRows == 0) {
+//		System.out.println("number of rows: " + numberOfRows);
+//		
+//		insertProduct("", 1.30);
+//		insertProduct("", 1.20);
+//		insertProduct("", 2.30);
+//		insertProduct("", 2.10);
+//		insertProduct("", 2.40);
+//		
+//		}
+//	}
+//	
+//	
+//	public void insertProduct (String productName, 	Double productPrice) {
+//		
+//		System.out.println("Insert product " + productName + "with price " + productPrice);
+//		
+//		try {
+//			PreparedStatement st = conn.prepareStatement("INSERT INTO PRODUCTS (" +
+//					 "product_name, product_price) " +
+//					 "VALUES(?,?)");
+//			st.setString(1, productName);
+//			st.setDouble(2, productPrice);
+//			st.executeUpdate();
+//			st.close();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//	public List selectAllProducts() {
+//		
+//		List<Product> allProductsList = new ArrayList<>();
+//		
+//		try {
+//			PreparedStatement st = conn.prepareStatement("SELECT product_id, product_name, product_price FROM products");
+//			ResultSet rs = st.executeQuery();
+//			
+//			while (rs.next()) {
+//				Long productId = rs.getLong(1);
+//				String productName = rs.getString(2);
+//				Double productPrice = rs.getDouble(3);
+//				
+//				Product tempProduct = new Product(productId, productName, productPrice);
+//				
+//				allProductsList.add(tempProduct);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return allProductsList;
+//	}
+//	
+//	public void updateProduct(long productId, String productName, double productPrice) {
+//		
+//		System.out.println("Updating item number " + productId + " with name = " + productName + 
+//				" price = " + productPrice);
+//		
+//		try {
+//			
+//			if(!(productName.equals(""))) {
+//					
+//			PreparedStatement st = conn.prepareStatement("UPDATE products SET product_name=? WHERE product_id=?");
+//			
+//			st.setString(1, productName);
+//			st.setLong(2, productId);
+//			st.executeUpdate();
+//			st.close();
+//			}
+//			
+//			if(productPrice != -1.0) {
+//				PreparedStatement st = conn.prepareStatement("UPDATE products SET product_price=? WHERE product_id=?");
+//
+//						st.setDouble(1, productPrice);
+//						st.setLong(2, productId);
+//						st.executeUpdate();
+//						st.close();
+//			}
+//			
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+	
+}
