2c99dd57b2a43a55a6c99a1b01cfc25fa9020eff
Simon Gustafson <Simon@Simons-MacBook-Pro.local>
2019-05-01 17:52:48 +0200

    Lagt till kommentarsfunktion på titelsida

--- Hemsidan/js/script.js
+++ Hemsidan/js/script.js
@@ -4,6 +4,34 @@ $(document).ready(function(){
     });
 });
 
+function render(data){
+    var html = "<div class='commentBox'><div class='leftPanelImg'><img src='https://via.placeholder.com/100?text='></div><div class='rightPanel'><span>"+data.name+"</span><div class='date'>"+data.date+"</div><p>"+data.body+"</p></div></div>";
+    $('#container').append(html);  
+}
+
+$(document).ready(function(){  
+    
+    var comment = [];
+
+        for(var i=0;i<comment.lenght;i++){
+            render(comment[i]);
+        }
+
+    $('#addComment').click(function(){
+        var addObj = {
+            "name": $('#name').val(),
+            "date": $('#date').val(),
+            "body": $('#bodytext').val()
+        };
+        console.log(addObj);
+        comment.push(addObj);
+        render(addObj);    
+        $('#name').val(' ');
+        $('#date').val(' ');
+        $('#bodytext').val(' ')
+
+    });
+});
 
 $(document).ready(function(){
     $('#menu > ul >li').on('click', function(){
@@ -11,6 +39,8 @@ $(document).ready(function(){
         $(this).addClass('active');
     });
 });
+
+
 
 
 
