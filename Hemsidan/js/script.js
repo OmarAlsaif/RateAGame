975fd0a2ec864a66b2e40ce034fe5088bb871ed6
OmarAlsaif <omaralsaif1996@gmail.com>
2019-05-05 09:43:02 +0200

    tog bort onödig kod

--- Hemsidan/js/script.js
+++ Hemsidan/js/script.js
@@ -89,25 +89,3 @@ slideContainer.addEventListener('transitionend', ()=>{
 });
 
 
-
-
-
-//Rating system
-
-$(document).ready(function(){
-    // Check Radio-box
-    $(".rating input:radio").attr("checked", false);
-
-    $('.rating input').click(function () {
-        $(".rating span").removeClass('checked');
-        $(this).parent().addClass('checked');
-    });
-
-    $('input:radio').change(
-      function(){
-        var userRating = this.value;
-    }); 
-});
-
-
-
