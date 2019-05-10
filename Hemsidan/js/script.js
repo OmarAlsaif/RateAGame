a7d160b94400c7cc099bc9ebb0d4637c227e8cb4
OmarAlsaif <omaralsaif1996@gmail.com>
2019-05-10 22:04:17 +0200

    FIxat så att man inte kan skriva olika lösenord vid registreringen

--- Hemsidan/js/script.js
+++ Hemsidan/js/script.js
@@ -88,18 +88,3 @@ slideContainer.addEventListener('transitionend', ()=>{
 
 });
 
-var txtPass1 = document.getElementById("txtPass1")
-  , txtPass2 = document.getElementById("txtPass2");
-
-function validatePassword(){
-  if(txtPass1.value != txtPass2.value) {
-    txtPass2.setCustomValidity("Passwords Don't Match");
-  } else {
-    txtPass2.setCustomValidity('');
-  }
-}
-
-txtPass1.onchange = validatePassword;
-txtPass2.onkeyup = validatePassword;
-
-
